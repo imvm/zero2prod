0a76da2fe82aa5ae7935d4048fc1b8c3df10af8b
iamm <iamm@yelp.com>
2021-11-10 09:17:47 +0000

    Insert emails in database
    - Add logging

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 //! src/main.rs
+use env_logger::Env;
+use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection_pool)?.await
 }
